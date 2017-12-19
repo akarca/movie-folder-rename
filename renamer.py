03cd7a72d877ebda1e7ea7db2d0b7046e2d4b887
Serdar Akarca <serdar@yuix.org>
2017-12-19 07:53:54 +0300

    add imdbpy search

--- renamer.py
+++ renamer.py
@@ -43,58 +43,7 @@ def get_name(path):
 
     return path
 
-def imdb_url(path, year=None):
-    # url = 'http://www.imdb.com/find?q=' + '+'.join(path.split(' '))
-
-    if year:
-        url = 'http://www.imdb.com/search/title?release_date=%s,%s&title=%s&view=simple' % (year, year, path)
-    else:
-        url = 'http://www.imdb.com/search/title?title=%s&view=simple' % path
-
-    url = unicodedata.normalize('NFKD', url).encode('ascii', errors='ignore').decode('ascii')
-
-    return url
-
-def crawl_imdb(url):
-    request = urllib.request.Request(url, None, {'Accept-Language': 'en-US,en;q=0.8,de;q=0.6'})
-    response = urllib.request.urlopen(request)
-    content = response.read()
-    text = content.decode('utf-8-sig', errors='ignore')
-
-    return text
-
-def parse_title(html):
-    matches = re.search('"result_text">[^>]+>(?P<title>[^<]*)', html)
-
-    if not matches:
-        return None
-
-    title = matches.group('title')
-    title = title.strip()
-    if len(title) > 3 and re.match('[0-9A-Za-z-,!?]+', title):
-        return title
-
-    return None
-
-def parse_year(html):
-    matches = re.search('"result_text">[^>]+>[^>]+> (?P<text>[^<]+) <', html)
-
-    if not matches:
-        return None
-
-    text = matches.group('text')
-    matches = re.search('(?P<year>[12][0-9]{3})', text)
-
-    if matches:
-        return matches.group('year')
-
-    return None
-
 def fetch_movie(name, year=None):
-    #url = imdb_url(name, year=year)
-    #html = crawl_imdb(url)
-    #title = parse_title(html)
-    #year = parse_year(html)
     ia = imdb.IMDb()
     results = ia.search_movie(name)
 
@@ -103,9 +52,10 @@ def fetch_movie(name, year=None):
             if result.data["year"] == year:
                 return result.data["title"], result.data["year"]
 
-    return results[0].data["title"], results[0].data["year"]
+    if results and len(results):
+        return results[0].data["title"], results[0].data["year"]
 
-
+    return None, None
 
 def strip_after(string, character):
     position = string.rfind(character)
