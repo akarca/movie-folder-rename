92a0d2f37b6b78e2bb1c4227e5957d62fcb31899
Serdar Akarca <serdar@yuix.org>
2017-12-22 19:00:27 +0300

    single folder rename

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+import sys
+
 from renamer import folders, get_name, fetch_movie, compare_titles, user_decision, rename, print_line, get_year, rename_folder
 
 print_line('Input folder', 'Year', 'Title', 'Output folder', 'Action')
 print('-' * (31 * 3 + 14))
 
-for folder in folders():
-    rename_folder(folder)
+args = sys.argv
+if len(args):
+    for folder in args:
+        rename_folder(folder)
+else:
+    for folder in folders():
+        rename_folder(folder)
 
 print('')
 print('DONE')
